2ddebc62b0c9403ebd266166d7325a4aa9e4c938
yeahbutstill <mayadani28@gmail.com>
2023-10-12 08:12:13 +0700

    day 1

---
+++ jajalrust/hello_cargo/target/debug/deps/hello_cargo-cc7d7232fd6d96f1.d
@@ -0,0 +1,5 @@
+/home/yeahbutstill/IdeaProjects/github/RUST/buku-pemrograman-rust/jajalrust/hello_cargo/target/debug/deps/hello_cargo-cc7d7232fd6d96f1.rmeta: src/main.rs
+
+/home/yeahbutstill/IdeaProjects/github/RUST/buku-pemrograman-rust/jajalrust/hello_cargo/target/debug/deps/hello_cargo-cc7d7232fd6d96f1.d: src/main.rs
+
+src/main.rs:
---
+++ jajalrust/hello_cargo/target/debug/deps/hello_cargo-dd40b6dad37d3fc9.d
@@ -0,0 +1,5 @@
+/home/yeahbutstill/IdeaProjects/github/RUST/buku-pemrograman-rust/jajalrust/hello_cargo/target/debug/deps/hello_cargo-dd40b6dad37d3fc9.rmeta: src/main.rs
+
+/home/yeahbutstill/IdeaProjects/github/RUST/buku-pemrograman-rust/jajalrust/hello_cargo/target/debug/deps/hello_cargo-dd40b6dad37d3fc9.d: src/main.rs
+
+src/main.rs:
